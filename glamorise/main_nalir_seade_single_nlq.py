9bb6bde4ada1f9b7f33edf503f6a5a2dfe1d30c6
Antônio <anttoniocl@yahoo.com>
2022-09-19 15:52:29 -0300

    Alteração no nome de alguns arquivos e ajuste no arquivo 'glamorise_nalir_pt.ipynb'

--- glamorise/main_nalir_seade_single_nlq.py
+++ glamorise/main_nalir_seade_single_nlq.py
@@ -6,14 +6,14 @@ from glamorise_nlidb import GlamoriseNlidb
 import main_common as mc
 from xml.etree.ElementTree import fromstring
 
-with open('./config/environment/nalir_tokens_seade.xml') as xml_file:
+with open('config/environment/nalir_tokens_adapted.xml') as xml_file:
     nalir_tokens = xml_file.read()
     nalir_tokens = fromstring(nalir_tokens)
 
 with open('./config/environment/nalir_seade_db.json') as json_file:
     config_db = json_file.read()
 
-with open('./config/environment/glamorise_nalir_seade.json') as json_file:
+with open('config/environment/glamorise_nalir_adapted.json') as json_file:
     config_glamorise = json_file.read()
 
 with open('./config/environment/glamorise_interface_nalir_seade.json') as json_file:
